47d78f55eebe8cdcd7e8392570e4767ddac5666a
Alex Livenson <alexlivenson@gmail.com>
2017-02-19 22:37:54 -0800

    Alex L - Added registration endpoint

--- main.go
+++ main.go
@@ -1,13 +1,70 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
+
+	"log"
+
+	"encoding/json"
+
+	"strings"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	router := mux.NewRouter()
+	router.Methods(http.MethodPost).Path("/api/registration").HandlerFunc(registerationHandler)
+
 	fs := http.FileServer(http.Dir("./public"))
 	router.PathPrefix("/").Handler(fs)
+
+	err := http.ListenAndServe(":9000", router)
+	if err != nil {
+		log.Fatal("ListendAndServe:", err)
+	}
+}
+
+func writeBadRequest(w http.ResponseWriter, e error) {
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write([]byte(e.Error()))
+}
+
+func registerationHandler(w http.ResponseWriter, r *http.Request) {
+	type Registration struct {
+		FirstName      string `json:"first_name"`
+		LastName       string `json:"last_name"`
+		Email          string `json:"email"`
+		Password       string `json:"password"`
+		PhoneValue     string `json:"phone_value"`
+		PhoneCountryID int    `json:"phone_country_id"`
+	}
+
+	var registration Registration
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&registration)
+
+	if err != nil {
+		writeBadRequest(w, err)
+		return
+	}
+
+	val, _ := json.Marshal(registration)
+	resp, err := http.Post("http://localhost:8000/registration", "application/json", strings.NewReader(string(val)))
+	defer resp.Body.Close()
+
+	if err != nil {
+		writeBadRequest(w, err)
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		writeBadRequest(w, err)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write(body)
 }
